54e3d948f2604898e7d382046616a04f3537f867
zone <zonetw.web@gmail.com>
2019-02-03 21:25:42 +0800

    Promise will call resolve or reject when finish

--- __tests__/Promise.ts
+++ __tests__/Promise.ts
@@ -9,4 +9,23 @@ describe("1. Basic Test", ()=>{
         let promise = new Promise((resolve, reject)=>{});
         expect(typeof promise.then).toEqual("function");
     });
+
+    it("Promise will call resolve function when fulfilled", (done)=>{
+        let promise = new Promise((resolve, reject)=>{
+            process.nextTick(()=>{
+                resolve();
+                done();
+            });
+        });
+    });
+
+    it("Promise will call reject function when failed", (done)=>{
+        let promise = new Promise((resolve, reject)=>{
+            process.nextTick(()=>{
+                reject();
+                done();
+            });
+        });
+    });
+    
 });
--- src/Promise.ts
+++ src/Promise.ts
@@ -1,8 +1,12 @@
 
 export class Promise{
     constructor(executor: (callbackWhenResolve: (result?: any)=>void, callbackWhenReject: (reject?: any)=>void)=>void){
+        executor(this.resolve, this.reject);
+    }
+    private resolve(){
+    }
+    private reject(){
     }
     then(){
-
     }
 }
