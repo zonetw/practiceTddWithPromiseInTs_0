ae9de14f2a3e141cb76ae23d7ed4e60f3612108a
zone <zonetw.web@gmail.com>
2019-02-03 19:49:37 +0800

    Promise receive a function as parameter
    which receives two parameters: resolve and reject function

--- __tests__/Promise.ts
+++ __tests__/Promise.ts
@@ -1,8 +1,12 @@
 import {Promise} from "../src/Promise";
 
 describe("1. Basic Test", ()=>{
-    it("1.1. promise is an object or function with a then method whose behavior conforms to this specification", ()=>{
-        let promise = new Promise();
+    it("Promise receive a function, which receives two parameters: resolve and reject function, as parameter", ()=>{
+        let promise = new Promise((resolve, reject)=>{});
+    });
+
+    it("Promise is an object or function with a then method whose behavior conforms to this specification", ()=>{
+        let promise = new Promise((resolve, reject)=>{});
         expect(typeof promise.then).toEqual("function");
     });
 });
--- src/Promise.ts
+++ src/Promise.ts
@@ -1,7 +1,6 @@
 
 export class Promise{
-    constructor(){
-
+    constructor(executor: (callbackWhenResolve: (result?: any)=>void, callbackWhenReject: (reject?: any)=>void)=>void){
     }
     then(){
 
